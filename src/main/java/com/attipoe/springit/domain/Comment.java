dd5b100c3b6a111a3ac54b86b285d66584536065
simonszy <simons.attipoe@gmail.com>
2022-06-16 16:04:08 -0400

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/attipoe/springit/domain/Comment.java
@@ -0,0 +1,20 @@
+package com.attipoe.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+    // link
+}
